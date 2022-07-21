cdd2094d9be21df7b64a607231b5233f1deab079
mikhalchukvladislav <mikhalchukvladislav@ya.ru>
2022-07-21 18:07:38 +0300

    edit task3

--- task3/main.py
+++ task3/main.py
@@ -1,7 +1,8 @@
-from task2 import appearance
+from task import appearance
 from test_data import tests
 
 if __name__ == '__main__':
-   for i, test in enumerate(tests):
-       test_answer = appearance(test['data'])
-       assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    for i, test in enumerate(tests):
+        test_answer = appearance(test['data'])
+        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+        print(f'Test case {i}, {test_answer} sec')
